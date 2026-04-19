e8ca68aeb6aff5ffaefe5c53e4f75cc4dad1bd30
HARDIK-WEB-OSS <agarwalhardik549@gmail.com>
2026-04-19 08:48:24 +0000

    add .gitignore, real benchmark results, remove build artifacts

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/agarw/Downloads/order-book-engine-cpp/order-book-engine-cpp
-# Build directory: /mnt/c/Users/agarw/Downloads/order-book-engine-cpp/order-book-engine-cpp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(matching_tests "/mnt/c/Users/agarw/Downloads/order-book-engine-cpp/order-book-engine-cpp/build/test_matching")
-set_tests_properties(matching_tests PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/Users/agarw/Downloads/order-book-engine-cpp/order-book-engine-cpp/CMakeLists.txt;51;add_test;/mnt/c/Users/agarw/Downloads/order-book-engine-cpp/order-book-engine-cpp/CMakeLists.txt;0;")
